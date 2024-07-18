69de1a98f22060a7b57c1e5640b40ca4de87398d
Aurelos1 <147404299+Aurelos1@users.noreply.github.com>
2024-07-18 10:04:33 +0200

    list -> loisir

--- app/lib/screens/list_page.dart
+++ app/lib/screens/list_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:app/app.classname/header.dart';
 import 'package:app/app.classname/footer.dart';
+import 'package:app/screens/loisirDetail_screen.dart'; // Assurez-vous d'importer le fichier de détail
 
 class HeroListPage extends StatelessWidget {
   const HeroListPage({Key? key}) : super(key: key);
@@ -26,7 +27,17 @@ class HeroListPage extends StatelessWidget {
               return InkWell(
                 onTap: () {
                   Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => SecondPage(heroTag: index)));
+                      builder: (context) => LoisirDetailScreen(
+                            loisir: {
+                              'id': 'fake_id_$index', // Fausse ID
+                              'image': _images[index],
+                              'titre': 'Titre $index',
+                              'description': 'Description du loisir $index',
+                              'categorie': 'Catégorie $index',
+                              'note': 4.5,
+                              'date_publication': '2024-07-15'
+                            },
+                          )));
                 },
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -44,9 +55,10 @@ class HeroListPage extends StatelessWidget {
                       ),
                       const SizedBox(width: 10),
                       Expanded(
-                          child: Text(
-                        'Title: $index',
-                      )),
+                        child: Text(
+                          'Title: $index',
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -54,39 +66,6 @@ class HeroListPage extends StatelessWidget {
             },
           ),
         ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  final int heroTag;
-
-  const SecondPage({Key? key, required this.heroTag}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Hero ListView Page 2")),
-      body: Column(
-        children: [
-          Expanded(
-            child: Center(
-              child: Hero(
-                tag: heroTag,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Image.network(_images[heroTag]),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Text(
-              "Content goes here",
-            ),
-          )
-        ],
       ),
     );
   }
