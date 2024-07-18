24f62d3811d687418217a6d374ac48502b1969f5
Aurelos1 <147404299+Aurelos1@users.noreply.github.com>
2024-07-18 12:45:15 +0200

    list -> loisir

--- app/lib/app.classname/footer.dart
+++ app/lib/app.classname/footer.dart
@@ -1,5 +1,6 @@
 import 'package:app/app.API/API_loisir.dart';
 import 'package:app/components/create_loisir_form.dart';
+import 'package:app/screens/home_page.dart'; // Assurez-vous d'importer la HomePage
 import 'package:flutter/material.dart';
 
 class FooterWidget extends StatefulWidget {
@@ -12,6 +13,20 @@ class FooterWidget extends StatefulWidget {
 class _FooterWidgetState extends State<FooterWidget> {
   int currentPageIndex = 0;
 
+  void _navigateToPage(int index) {
+    switch (index) {
+      case 0:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+        break;
+      case 1:
+        // Ajoutez la navigation vers la page de loisirs si nécessaire
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -21,6 +36,7 @@ class _FooterWidgetState extends State<FooterWidget> {
           onDestinationSelected: (int index) {
             setState(() {
               currentPageIndex = index;
+              _navigateToPage(index);
             });
           },
           indicatorColor: const Color(0xFF806491),
@@ -32,8 +48,7 @@ class _FooterWidgetState extends State<FooterWidget> {
               label: 'Accueil',
             ),
             NavigationDestination(
-              selectedIcon: Icon(Icons.book,
-                  color: Colors.white), // Appropriate icon for loisirs
+              selectedIcon: Icon(Icons.book, color: Colors.white),
               icon: Icon(Icons.book_outlined, color: Colors.white),
               label: 'Loisirs',
             ),
--- app/lib/screens/list_page.dart
+++ app/lib/screens/list_page.dart
@@ -3,8 +3,89 @@ import 'package:app/app.classname/header.dart';
 import 'package:app/app.classname/footer.dart';
 import 'package:app/screens/loisirDetail_screen.dart'; // Assurez-vous d'importer le fichier de détail
 
-class HeroListPage extends StatelessWidget {
+class HeroListPage extends StatefulWidget {
   const HeroListPage({Key? key}) : super(key: key);
+
+  @override
+  _HeroListPageState createState() => _HeroListPageState();
+}
+
+class _HeroListPageState extends State<HeroListPage> {
+  List<Map<String, dynamic>> _loisirs = [
+    {
+      'id': '1',
+      'image':
+          'https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      'titre': 'a',
+      'description': 'Description du loisir 1',
+      'categorie': 'Catégorie 1',
+      'note': 4.5,
+      'date_publication': '2024-07-15'
+    },
+    {
+      'id': '2',
+      'image':
+          'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'titre': 'c',
+      'description': 'Description du loisir 2',
+      'categorie': 'Catégorie 2',
+      'note': 4.5,
+      'date_publication': '2024-07-14'
+    },
+    {
+      'id': '3',
+      'image':
+          'https://images.pexels.com/photos/273935/pexels-photo-273935.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'titre': 'b',
+      'description': 'Description du loisir 3',
+      'categorie': 'Catégorie 3',
+      'note': 4.5,
+      'date_publication': '2024-07-13'
+    },
+    {
+      'id': '4',
+      'image':
+          'https://images.pexels.com/photos/1591373/pexels-photo-1591373.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'titre': 'a',
+      'description': 'Description du loisir 4',
+      'categorie': 'Catégorie 4',
+      'note': 4.5,
+      'date_publication': '2024-07-12'
+    },
+    {
+      'id': '5',
+      'image':
+          'https://images.pexels.com/photos/462024/pexels-photo-462024.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'titre': 'e',
+      'description': 'Description du loisir 5',
+      'categorie': 'Catégorie 5',
+      'note': 4.5,
+      'date_publication': '2024-07-11'
+    },
+    {
+      'id': '6',
+      'image':
+          'https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'titre': 'c',
+      'description': 'Description du loisir 6',
+      'categorie': 'Catégorie 6',
+      'note': 4.5,
+      'date_publication': '2024-07-10'
+    },
+  ];
+
+  void _sortLoisirsByTitle() {
+    setState(() {
+      _loisirs.sort((a, b) => a['titre'].compareTo(b['titre']));
+    });
+  }
+
+  void _sortLoisirsByDate() {
+    setState(() {
+      _loisirs.sort(
+          (a, b) => b['date_publication'].compareTo(a['date_publication']));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,61 +102,73 @@ class HeroListPage extends StatelessWidget {
       body: Center(
         child: Container(
           constraints: const BoxConstraints(maxWidth: 500),
-          child: ListView.builder(
-            itemCount: _images.length,
-            itemBuilder: (BuildContext context, int index) {
-              return InkWell(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => LoisirDetailScreen(
-                            loisir: {
-                              'id': 'fake_id_$index', // Fausse ID
-                              'image': _images[index],
-                              'titre': 'Titre $index',
-                              'description': 'Description du loisir $index',
-                              'categorie': 'Catégorie $index',
-                              'note': 4.5,
-                              'date_publication': '2024-07-15'
-                            },
-                          )));
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Hero(
-                        tag: index,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            _images[index],
-                            width: 200,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: Text(
-                          'Title: $index',
-                        ),
-                      ),
-                    ],
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: _sortLoisirsByTitle,
+                    child: const Text('a-z'),
                   ),
+                  ElevatedButton(
+                    onPressed: _sortLoisirsByDate,
+                    child: const Text('Date'),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _loisirs.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final loisir = _loisirs[index];
+                    return InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => LoisirDetailScreen(
+                                  loisir: loisir,
+                                )));
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Hero(
+                              tag: loisir['id'],
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: Image.network(
+                                  loisir['image'],
+                                  width: 200,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    loisir['titre'],
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    'Date: ${loisir['date_publication']}',
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
-final List<String> _images = [
-  'https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-  'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/273935/pexels-photo-273935.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/1591373/pexels-photo-1591373.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/462024/pexels-photo-462024.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-];
