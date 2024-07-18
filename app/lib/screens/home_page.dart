84ac9f259d29aefe53ff85bf3b603845146fe120
Aurelos1 <147404299+Aurelos1@users.noreply.github.com>
2024-07-18 09:25:29 +0200

    listview

--- app/lib/screens/home_page.dart
+++ app/lib/screens/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:app/app.classname/header.dart';
 import 'package:app/app.classname/footer.dart';
-import 'package:app/app.classname/search_bar.dart'; // Import the SearchBar widget
+import 'package:app/app.classname/search_bar.dart';
+import 'package:app/components/create_loisir_form.dart';
+import 'package:app/screens/list_page.dart'; // Import the SearchBar widget
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -36,32 +38,124 @@ class _HomePageState extends State<HomePage> {
             Expanded(
               child: Column(
                 children: [
-                  // `rect1` occupe 1/3 de l'espace vertical
+                  // `rect1` occupe 1/4 de l'espace vertical
                   Container(
                     color: Colors.grey[300], // Couleur de fond de rect1
                     height: MediaQuery.of(context).size.height /
-                        3, // Hauteur de 1/3 de l'espace disponible
-                    child: Center(
-                      child: Text(
-                        'rect1',
-                      ),
+                        4, // Hauteur de 1/4 de l'espace disponible
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Top 5 note :',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              children: [
+                                Image.asset('images/avatar.webp',
+                                    width: 50, height: 50),
+                                const Text('Avatar'),
+                                const Text('4.5'),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset('images/goldenye.webp',
+                                    width: 50, height: 50),
+                                const Text('Golden Eye'),
+                                const Text('4.3'),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset('images/matrixx.webp',
+                                    width: 50, height: 50),
+                                const Text('Matrix'),
+                                const Text('4.7'),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset('images/meninblack.webp',
+                                    width: 50, height: 50),
+                                const Text('Men in Black'),
+                                const Text('4.4'),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset('images/titanic.webp',
+                                    width: 50, height: 50),
+                                const Text('Titanic'),
+                                const Text('4.6'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
 
-                  // `rect2` occupe 2/3 de l'espace vertical
+                  // `rect2` occupe 3/4 de l'espace vertical
                   Expanded(
                     child: Container(
                       color: Colors.grey[200], // Couleur de fond de rect2
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'rect2',
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  // Naviguer vers la classe CreateLoisirForm
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => HeroListPage(),
+                                    ),
+                                  );
+                                },
+                                child: const Text('filtre ->'),
+                              ),
                             ),
-                            // Ajoutez d'autres widgets ici si nécessaire
-                          ],
-                        ),
+                          ),
+                          Expanded(
+                            child: GridView.count(
+                              crossAxisCount: 2,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10,
+                              padding: const EdgeInsets.all(8.0),
+                              children: List.generate(4, (index) {
+                                return Column(
+                                  children: [
+                                    Container(
+                                      color: Colors.grey,
+                                      height: 100,
+                                      child: Image.asset(
+                                        'images/avatar.webp', // Replace with your image paths
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      'Image ${index + 1}', // Replace with the actual name
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      '4.${index + 1}', // Replace with the actual rating
+                                    ),
+                                  ],
+                                );
+                              }),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
