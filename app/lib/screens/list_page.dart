a2335bb03ab40de46efbb5caaa271c604c9c85e1
Aurelos1 <147404299+Aurelos1@users.noreply.github.com>
2024-07-19 15:08:58 +0200

    date

--- app/lib/screens/list_page.dart
+++ app/lib/screens/list_page.dart
@@ -82,91 +82,92 @@ class _ListPageState extends State<ListPage> {
   }
 
   @override
-Widget build(BuildContext context) {
-  final ThemeData theme = Theme.of(context);
-  return Scaffold(
-    appBar: AppBar(
-      title: HeaderWidget(
-        title: 'Culture Vibe',
-        imagePath: 'images/logo.png',
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: HeaderWidget(
+          title: 'Culture Vibe',
+          imagePath: 'images/logo.png',
+        ),
+        backgroundColor: const Color(0xFF2F70AF),
       ),
-      backgroundColor: const Color(0xFF2F70AF),
-    ),
-    bottomNavigationBar: FooterWidget(currentPage: 'Loisirs'),
-    body: Center(
-      child: Container(
-        constraints: const BoxConstraints(maxWidth: 500),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  onPressed: _sortLoisirsByTitle,
-                  child: const Text('a-z'),
-                ),
-                ElevatedButton(
-                  onPressed: _sortLoisirsByDate,
-                  child: const Text('Date'),
-                ),
-              ],
-            ),
-            Expanded(
-              child: _isLoading
-                  ? Center(child: CircularProgressIndicator())
-                  : ListView.builder(
-                      itemCount: _loisirs.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        final loisir = _loisirs[index];
-                        return InkWell(
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => LoisirDetailScreen(
-                                      loisir: loisir,
-                                    )));
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                Hero(
-                                  tag: loisir['id'],
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(8),
-                                    child: Image.network(
-                                      loisir['image'],
-                                      width: 100,
-                                      height: 100,
-                                      fit: BoxFit.cover,
+      bottomNavigationBar: FooterWidget(currentPage: 'Loisirs'),
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 500),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: _sortLoisirsByTitle,
+                    child: const Text('a-z'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _sortLoisirsByDate,
+                    child: const Text('Date'),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: _isLoading
+                    ? Center(child: CircularProgressIndicator())
+                    : ListView.builder(
+                        itemCount: _loisirs.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final loisir = _loisirs[index];
+                          return InkWell(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => LoisirDetailScreen(
+                                        loisir: loisir,
+                                      )));
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Hero(
+                                    tag: loisir['id'],
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(8),
+                                      child: Image.network(
+                                        loisir['image'],
+                                        width: 100,
+                                        height: 100,
+                                        fit: BoxFit.cover,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                const SizedBox(width: 10),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        loisir['titre'],
-                                      ),
-                                      const SizedBox(height: 5),
-                                      Text(
-                                        'Date: ${DateFormat('dd/MM/yyyy').format(loisir['date_publication'])}',
-                                      ),
-                                    ],
+                                  const SizedBox(width: 10),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          loisir['titre'],
+                                        ),
+                                        const SizedBox(height: 5),
+                                        Text(
+                                          'Date: ${DateFormat('dd/MM/yyyy').format(loisir['date_publication'])}',
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-            ),
-          ],
+                          );
+                        },
+                      ),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
