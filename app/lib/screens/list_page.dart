3bf26b716eba7dbefb30f8fd63f933157af20887
Aurelos1 <147404299+Aurelos1@users.noreply.github.com>
2024-07-19 15:08:05 +0200

    date

--- app/lib/screens/list_page.dart
+++ app/lib/screens/list_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:app/app.classname/header.dart';
 import 'package:app/app.classname/footer.dart';
+import 'package:app/screens/loisirDetail_screen.dart'; // Assurez-vous d'importer le fichier de détail
 import 'package:app/app.API/API_loisir.dart';
-import 'package:intl/intl.dart';
-import 'package:app/components/sort_filter_buttons.dart';
-import 'package:app/components/loisir_item.dart';
+import 'package:intl/intl.dart'; // Import intl for date formatting
 
 class ListPage extends StatefulWidget {
   const ListPage({Key? key}) : super(key: key);
@@ -15,38 +14,32 @@ class ListPage extends StatefulWidget {
 
 class _ListPageState extends State<ListPage> {
   List<Map<String, dynamic>> _loisirs = [];
-  List<Map<String, dynamic>> _allLoisirs = [];
-  List<String> _categories = [];
   bool _isLoading = true;
   String _sortCriteria = 'titre'; // Initial sort criteria
-  String? _selectedCategory = 'Toutes les catégories';
 
   @override
   void initState() {
     super.initState();
     _fetchLoisirs();
-    _fetchCategories();
   }
 
   Future<void> _fetchLoisirs() async {
     setState(() {
       _isLoading = true;
     });
-
     try {
       var loisirs = await LoisirApi.getAllLoisirs();
       setState(() {
-        _allLoisirs = loisirs
+        _loisirs = loisirs
             .map<Map<String, dynamic>>((loisir) => {
                   'id': loisir['id'].toString(),
                   'image': loisir['image'],
                   'titre': loisir['titre'],
                   'description': loisir['description'],
-                  'categorie': loisir['category_nom'],
+                  'categorie': loisir['categorie'],
                   'note': loisir['note'],
-                  'moyen_note': loisir['moyen_note'],
-                  'date_publication': DateFormat('dd/MM/yyyy')
-                      .format(DateTime.parse(loisir['date_publication']))
+                  'date_publication':
+                      DateTime.parse(loisir['date_publication']),
                 })
             .toList();
         _sortLoisirs();
@@ -60,36 +53,16 @@ class _ListPageState extends State<ListPage> {
     }
   }
 
-  Future<void> _fetchCategories() async {
-    try {
-      var categories = await LoisirApi.getAllCategories();
-      setState(() {
-        _categories = ['Toutes les catégories'];
-        _categories.addAll(
-            categories.map<String>((cat) => cat['nom'].toString()).toList());
-      });
-    } catch (e) {
-      print('Erreur lors de la récupération des catégories: $e');
-    }
-  }
-
   void _sortLoisirs() {
     setState(() {
-      _loisirs = List.from(_allLoisirs);
-
       if (_sortCriteria == 'titre') {
         _loisirs.sort((a, b) => a['titre'].compareTo(b['titre']));
       } else if (_sortCriteria == 'date') {
-        _loisirs.sort(
-            (a, b) => b['date_publication'].compareTo(a['date_publication']));
-      }
-
-      if (_selectedCategory != null &&
-          _selectedCategory!.isNotEmpty &&
-          _selectedCategory != 'Toutes les catégories') {
-        _loisirs = _loisirs
-            .where((loisir) => loisir['categorie'] == _selectedCategory)
-            .toList();
+        _loisirs.sort((a, b) {
+          DateTime dateA = a['date_publication'];
+          DateTime dateB = b['date_publication'];
+          return dateB.compareTo(dateA); // Tri décroissant
+        });
       }
     });
   }
@@ -108,52 +81,92 @@ class _ListPageState extends State<ListPage> {
     });
   }
 
-  void _filterByCategory(String? category) {
-    setState(() {
-      _selectedCategory = category;
-      _sortLoisirs();
-    });
-  }
-
   @override
-  Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: HeaderWidget(
-          title: 'Culture Vibe',
-          imagePath: 'images/logo.png',
-        ),
-        backgroundColor: const Color(0xFF806491),
+Widget build(BuildContext context) {
+  final ThemeData theme = Theme.of(context);
+  return Scaffold(
+    appBar: AppBar(
+      title: HeaderWidget(
+        title: 'Culture Vibe',
+        imagePath: 'images/logo.png',
       ),
-      bottomNavigationBar: FooterWidget(currentPage: 'Loisirs'), // Passer le nom de la page actuelle
-      body: Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: 500),
-          child: Column(
-            children: [
-              SortFilterButtons(
-                selectedCategory: _selectedCategory,
-                categories: _categories,
-                onSortByTitle: _sortLoisirsByTitle,
-                onSortByDate: _sortLoisirsByDate,
-                onCategorySelected: _filterByCategory,
-              ),
-              Expanded(
-                child: _isLoading
-                    ? const Center(child: CircularProgressIndicator())
-                    : ListView.builder(
-                        itemCount: _loisirs.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          final loisir = _loisirs[index];
-                          return LoisirItem(loisir: loisir);
-                        },
-                      ),
-              ),
-            ],
-          ),
+      backgroundColor: const Color(0xFF2F70AF),
+    ),
+    bottomNavigationBar: FooterWidget(currentPage: 'Loisirs'),
+    body: Center(
+      child: Container(
+        constraints: const BoxConstraints(maxWidth: 500),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: _sortLoisirsByTitle,
+                  child: const Text('a-z'),
+                ),
+                ElevatedButton(
+                  onPressed: _sortLoisirsByDate,
+                  child: const Text('Date'),
+                ),
+              ],
+            ),
+            Expanded(
+              child: _isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : ListView.builder(
+                      itemCount: _loisirs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final loisir = _loisirs[index];
+                        return InkWell(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => LoisirDetailScreen(
+                                      loisir: loisir,
+                                    )));
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Hero(
+                                  tag: loisir['id'],
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(8),
+                                    child: Image.network(
+                                      loisir['image'],
+                                      width: 100,
+                                      height: 100,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        loisir['titre'],
+                                      ),
+                                      const SizedBox(height: 5),
+                                      Text(
+                                        'Date: ${DateFormat('dd/MM/yyyy').format(loisir['date_publication'])}',
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+            ),
+          ],
         ),
       ),
-    );
-  }
+    ),
+  );
 }
